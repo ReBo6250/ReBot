76a11c66abc48307b25f648af672c32af27fd1fc
ReBo6250 <restrainboy@gmail.com>
2023-07-21 10:26:23 +0800

    self-learning from each user

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
+// Discord
 require('dotenv').config();
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-const {Configuration, OpenAIApi} = require("openai")
-const configuration = new Configuration({ apiKey: process.env.apiKey })
-
-const openai = new OpenAIApi(configuration)
 const botPrefix = '-rebot ';
 
 const specialWordTriggers = ['haha', 'hahaha', 'sino ka', 'dn', 'marben', 'rebo', 'rebot', 'umalis', 'tae', 'bruh', 'wag', 'hoy', 'nigga', 'sad', 'sadge', 'hin', 'gomen', 'hbd', 'happy birthday', 'bertdey', 'berdei', 'bts', 'kpop', 'i love you', 'mahal kita', 'aishiteru', 'ha'];
 const gameWordTriggers = ['tara', 'game', 'raro', 'ra', 'g', 'laro', 'tara', 'what', 'wut', 'nani', 'set'];
-const questionWordTriggers = ['?','kailan', 'kelan', 'bat', 'ano', 'bakit', 'ket', 'anu', 'saan', 'pano', 'panu', 'anong', 'anung', 'paano', 'paanong', 'panung', 'panong', 'saan'];
+const questionWordTriggers = ['kailan', 'kelan', 'bat', 'ano', 'bakit', 'ket', 'anu', 'saan', 'pano', 'panu', 'anong', 'anung', 'paano', 'paanong', 'panung', 'panong', 'saan'];
 const insultWordTriggers = ['gae', 'gay', 'pink', 'yellow', 'red', 'black', 'violet', 'purple', 'pilay', 'mama', 'pangit', 'panget', 'bot', 'weak', 'talo'];
 const curseWordTriggers = ['fuck', 'gagi', 'gago', 'shit', 'tanga', 'putang ina', 'pota'];
 const greetingsWordTriggers = ['oi', 'hey', 'uy', 'hi', 'hello', 'balita', 'musta', 'ey'] ;
@@ -24,37 +21,8 @@ client.on('ready', () => {
   console.log(`${client.user.tag} is online`);
 });
 
-
-async function aiChat(message) {
-  try {
-    const completion = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: message.content,
-        max_tokens: 3000
-    })
-    message.reply(completion.data.choices[0].text)
-  }catch (e) {
-      console.log(e)
-  }
-}
-
-function switchCaseCommand(message) {
-  command = message.content.replace(botPrefix, '')
-
-  switch (command) {
-    case 'logout':
-      logOut(message);
-      break;
-  
-    default:
-      aiChat(message);
-      break;
-  }
-}
-
-
 client.on('messageCreate', (message) => {
-  if (message.content.startsWith(botPrefix)) {
+  if (message.content.startsWith(botPrefix.toLowerCase())) {
     switchCaseCommand(message);
   }
   else {
@@ -101,53 +69,142 @@ client.on('messageCreate', (message) => {
   }
 });
 
+function switchCaseCommand(message) {
+  command = message.content.toLowerCase().replace(botPrefix.toLowerCase(), '')
+
+  switch (command) {
+    case 'logout':
+      logOut(message);
+      break;
+  
+    default:
+      aiChat(message);
+      break;
+  }
+}
+
+
+
+
+// Firebase Database
+const admin = require('firebase-admin');
+const serviceAccount = require('./data/rebot-85-firebase-adminsdk-o3g9o-4753ebf10a.json');
+
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://rebot-85-default-rtdb.asia-southeast1.firebasedatabase.app"
+  });
+  const db = admin.database();
+
+// OpenAI
+const {Configuration, OpenAIApi} = require("openai")
+const configuration = new Configuration({ organization: process.env.org,  apiKey: process.env.apiKey })
+const openai = new OpenAIApi(configuration)
+
+
+// Functions
+async function aiChat(message) {
+  const messageContent = message.content.toLowerCase().replace(botPrefix, '');
+  const userId = message.author.id;
+  const currentTime = new Date().toLocaleString(); // Get the current date and time
+
+  try {
+    // Check if the user has any previous conversation
+    const conversationRef = db.ref('conversations/' + userId);
+    const conversationSnapshot = await conversationRef.once('value');
+    let conversationData = conversationSnapshot.val() ? conversationSnapshot.val().conversation : [];
+
+    // Add the user message to the conversation history with the current date and time
+    conversationData.push({ role: 'user', content: messageContent, timeSent: currentTime });
+
+    // Create a list of previous user messages as context for AI response
+    const context = conversationData.map(item => item.content).join('\n');
+
+    // Make an API call to OpenAI to get the chat completion response
+    const completion = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo-16k',
+      
+      messages: [
+        {role: "user", content: context}
+      ],
+      temperature: 0,
+      top_p: 1,
+      frequency_penalty: 1.0,
+      presence_penalty: 0,
+      max_tokens: 3000,
+    });
+
+    // Extract the response text from the completion
+    const botResponse = completion.data.choices[0].message;
+
+    // Add the bot's response to the conversation history with the current date and time
+    conversationData.push({ role: 'assistant', content: botResponse, timeSent: currentTime });
+
+    // Save the updated conversation data to Firebase Realtime Database
+    await conversationRef.set({ conversation: conversationData });
+
+    // Reply to the message with the bot's response
+    message.reply(botResponse);
+  } catch (error) {
+    console.error('Error in AI chat:', error);
+    return 'An error occurred while processing your message.';
+  }
+}
 
 
 
 
 
+// Function for replying to game word triggers
 function replyToGameWordTriggers(message, word) {
   const replyTexts = [ `${word} ka mag-isa`, `Gusto ko sana kaso ayaw ni JJ`, `Maghuhugas pa ko pinggan`, `${word} ka!`, `Set na!`, `${word}!` ]
 
   message.reply(replyTexts[Math.floor(Math.random() * replyTexts.length)]);
 }
 
+// Function for replying to question word triggers
 function replyToQuestionWordTriggers(message, word) {
   const replyTexts = [ `Ewan`, `${word}?`, `Gamit ka calcu`, `Alam ko pero di ko sasabihin`, `Sagot ko ${Math.floor(Math.random() * 100)}` ]
 
   message.reply(replyTexts[Math.floor(Math.random() * replyTexts.length)]);
 }
 
+// Function for replying to insult word triggers
 function replyToInsultWordTriggers(message, word) {
   const replyTexts = [ `Mama mo ${word}!`, `Mas ${word} ka!`, `Oof!` ]
 
   message.reply(replyTexts[Math.floor(Math.random() * replyTexts.length)]);
 }
 
+// Function for replying to curse word triggers
 function replyToCurseWordTriggers(message, word) {
   const replyTexts = [ `Oi bawal yan`, `Ah?`, `Mahal din kita`, `Mama may nagmumura o`, `Ano pong lingwahe yan?` ]
 
   message.reply(replyTexts[Math.floor(Math.random() * replyTexts.length)]);
 }
 
+// Function for replying to apologizing word triggers
 function replyToApologizingWordTriggers(message, word) {
   const replyTexts = [ `Pinapatawad na kita`, `Apology accepted!`, `Di kita mapapatawad!`, `${word} mo mama mo! >:(` ]
 
   message.reply(replyTexts[Math.floor(Math.random() * replyTexts.length)]);
 }
 
+// Function for replying to greetings word triggers
 function replyToGreetingsWordTriggers(message, word) {
   const replyTexts = [ `Hey`, `Uyyyy`, `Balita?`, `Musta na!`, `Long time no see! (?)` ]
 
   message.reply(replyTexts[Math.floor(Math.random() * replyTexts.length)]);
 }
 
+// Function for replying to parting word triggers
 function replyToPartingWordTriggers(message, word) {
   const replyTexts = [ `Bawal umales!`, `Umales panget!`, `Oi bawal umales!`, `Luh?!`, `Pinagbabawalan kitang umalis.` ]
 
   message.reply(replyTexts[Math.floor(Math.random() * replyTexts.length)]);
 }
 
+// Function for replying to special word triggers
 function replyToSpecialWordTriggers(message, word) {
   let replyTexts = [];
   switch (word.toLowerCase()) {
@@ -245,7 +302,7 @@ function replyWithPromise(message, content) {
 
 async function logOut (message) {
   try {
-    const sentMessage = await replyWithPromise(message, 'Bye!');
+    await replyWithPromise(message, 'Bye!');
     console.log(`${client.user.tag} is offline`);
     client.destroy();
   } catch (error) {
